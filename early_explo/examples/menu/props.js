b8e1478bab9da651a73fdf7b80b7f2af02e03d9c
Frank Elavsky <frankelavsky@gmail.com>
2024-07-05 08:30:57 -0400

    add chart and trim options

---
+++ early_explo/examples/menu/props.js
@@ -0,0 +1,127 @@
+let props = {
+    Comprehension: {
+        value: "",
+        available: true,
+        enabled: false,
+        "Alt text appearance": {
+            value: "",
+            available: true,
+            enabled: false
+        }
+    },
+    Text: {
+        value: "",
+        available: true,
+        enabled: false,
+        "Font Size": {
+            value: "",
+            available: true,
+            enabled: false,
+            Title: {
+                value: "",
+                available: true,
+                enabled: false
+            },
+            Subtitle: {
+                value: "",
+                available: true,
+                enabled: false
+            },
+            Tooltip: {
+                value: "",
+                available: true,
+                enabled: false
+            },
+            seriesLabel: {
+                value: "",
+                available: true,
+                enabled: false
+            }
+        },
+        "Font Weight": {
+            value: "",
+            available: true,
+            enabled: false,
+            title: {
+                value: "",
+                available: true,
+                enabled: false
+            },
+            subtitle: {
+                value: "",
+                available: true,
+                enabled: false
+            },
+            tooltip: {
+                value: "",
+                available: true,
+                enabled: false
+            },
+            seriesLabel: {
+                value: "",
+                available: true,
+                enabled: false
+            }
+        }
+    },
+    "Color and contrast": {
+        value: "",
+        available: true,
+        enabled: false,
+        "Text color": {
+            value: "",
+            available: true,
+            enabled: false
+        },
+        "Mark color": {
+            value: "",
+            available: true,
+            enabled: false
+        },
+        "Distinguish without color": {
+            value: "",
+            available: true,
+            enabled: false,
+            "Fill patterns": {
+                value: "",
+                available: true,
+                enabled: false
+            }
+        }
+    },
+    "Element size": {
+        value: "",
+        available: true,
+        enabled: false,
+        Lines: {
+            value: "",
+            available: true,
+            enabled: false,
+            Outlines : {
+                value: "",
+                available: true,
+                enabled: false
+            }
+        }
+    },
+    Audio: {
+        value: "",
+        available: false,
+        enabled: false
+    },
+    Motion: {
+        value: "",
+        available: true,
+        enabled: false
+    },
+    Interactivity: {
+        value: "",
+        available: true,
+        enabled: false,
+        "Pointer focus": {
+            value: "",
+            available: true,
+            enabled: false
+        }
+    }
+}
