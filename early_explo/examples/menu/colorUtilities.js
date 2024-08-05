78f547bd034e899a9303bf32f9c5f9f39818b414
Frank Elavsky <frankelavsky@gmail.com>
2024-08-04 22:50:16 -0400

    add color map for categories

--- early_explo/examples/menu/colorUtilities.js
+++ early_explo/examples/menu/colorUtilities.js
@@ -1,3 +1,14 @@
+let colorMap = {
+  'Geothermal': "#009c00", 
+  'Solar': "#009c00", 
+  'Wind': "#009c00", 
+  'Hydro': "#009c00", 
+  'Biomass': "#009c00", 
+  'Nuclear': "#1a8dff", 
+  'Coal': "#989898", 
+  'Natural Gas': "#1a8dff", 
+  'Petroleum': "#989898"
+}
 const componentToHex = (c) => {
   var hex = c.toString(16);
   return hex.length == 1 ? "0" + hex : hex;
